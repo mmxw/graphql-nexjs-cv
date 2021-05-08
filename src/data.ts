bfb92d258cb4b4623bb64687e09640a6e09e85b4
cara <xw33@outlook.com>
2021-05-08 23:59:19 +0100

    Add static data

---
+++ src/data.ts
@@ -0,0 +1,23 @@
+export const data = {
+  bio: {
+    name: "xuan wang",
+    tagline: "Full Stack Developer",
+    email: "xuanwangldn@gmail.com",
+    github: "https://github.com/mmxw",
+    linkedin: "https://linkedin.com/in/mmxw",
+    twitter: "https://twitter.com/xuanxw",
+  },
+  skills: ["Django", "React", "React Native", "TypeScript"],
+  positions: [
+    {
+      id: "1",
+      title: "Software Engineer",
+      company: "Natoora",
+      startDate: "2020-06-01",
+      endDate: null,
+      employmentType: "FULL_TIME",
+      location: "London, UK",
+      achievements: [],
+    },
+  ],
+};
